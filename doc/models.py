5e647da18bab4ab27e84f791fdb73a03edab78f9
Almin <akrbezlija@gmail.com>
2022-12-04 11:50:02 +0100

    Added styling on Home page, added my profile information

--- doc/models.py
+++ doc/models.py
@@ -8,6 +8,7 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(length=30), nullable=False, unique=True)
   email_address = db.Column(db.String(length=40), nullable=False, unique=True)
   password_hash = db.Column(db.String(length=50), nullable=False)
 
